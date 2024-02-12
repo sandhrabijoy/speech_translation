d3036f896e62091a990c8d3d32942ccee6a5c295
sandhrabijoy <sandhrasonabijoy@gmail.com>
2024-02-12 20:20:28 +0530

    Preprocess check using audio.wav format using different functions and parameters for audio

---
+++ audio.py
@@ -0,0 +1,7 @@
+import wave
+obj=wave.open("audio.wav","rb")
+print("Number of channels",obj.getnchannels())
+print("Sample width ",obj.getsampwidth())
+print("Frame rate",obj.getframerate())
+print("Number of frames",obj.getnframes())
+print("Parameters",obj.getparams())
