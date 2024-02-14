2d904497a0cef235cfc1e83f6bc8260c93fd6282
sandhrabijoy <sandhrasonabijoy@gmail.com>
2024-02-14 21:22:02 +0530

    Working with frames

--- audio.py
+++ audio.py
@@ -8,3 +8,7 @@ print("Parameters",obj.getparams())
 
 t_audio=obj.getnframes()/obj.getframerate()
 print(t_audio)
+
+frames=obj.readframes(-1)
+print(type(frames),type(frames[0]))
+print(len(frames)/2)
